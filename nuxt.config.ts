85a753dfb7958de3c5bf623f5e89989e48ca93a9
SuperKojima <hayato.kojima@steamship.co.jp>
2024-07-22 04:29:40 +0000

    nuxt-authいらんわこれ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@sidebase/nuxt-auth'
   ],
+
+  runtimeConfig: {
+    authSecret: process.env.NUXT_AUTH_SECRET,
+  },
+
   build: {
     transpile: [
       'trpc-nuxt'
     ]
   },
+
   typescript: {
     shim: false
-  }
+  },
+
+  compatibilityDate: '2024-07-22'
 })
